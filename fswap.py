8a5fba14909ff1621e34660bf50a952d25abad8f
Dima <dima.sapsay@gmail.com>
2019-12-02 11:49:45 +0200

    first commit

--- fswap.py
+++ fswap.py
@@ -1,2 +1,16 @@
-def fswap(filename1:str, filename2:str) -> None:
-    pass
+"""
+Swap each next pair of lines in the file
+"""
+
+
+def fswap(filename1: str, filename2: str) -> None:
+    """Swap"""
+    with open(filename1, 'rt') as rfile:
+        strg = rfile.read()
+
+    lst = strg.split('\n')[:-1]
+    for i in range(1, len(lst), 2):
+        lst[i-1], lst[i] = lst[i], lst[i-1]
+
+    with open(filename2, 'wt') as wfile:
+        wfile.write('\n'.join(lst)+'\n')
