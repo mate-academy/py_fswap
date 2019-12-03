75f4e240025f76b3dce79827f52034507d94efd9
koreyo <KoretsValentyn@gmail.com>
2019-12-03 17:47:55 +0200

    used with

--- fswap.py
+++ fswap.py
@@ -6,14 +6,13 @@ def fswap(filename1: str, filename2: str) -> None:
 
     file_read = open(filename1)
     file_write = open(filename2, 'w')
-    list_item = file_read.readlines()
-    if len(list_item) % 2:
-        end = len(list_item) - 1
-    else:
-        end = len(list_item)
-    list_item[1:end:2], list_item[:end:2] = list_item[:end:2], list_item[1:end:2]
-    for i in list_item:
-        file_write.write(i)
-
-    file_read.close()
-    file_write.close()
+    with open(filename1, 'rt') as file_read:
+        with open(filename2, 'wt') as file_write:
+            list_item = file_read.readlines()
+            if len(list_item) % 2:
+                end = len(list_item) - 1
+            else:
+                end = len(list_item)
+            list_item[1:end:2], list_item[:end:2] = list_item[:end:2], list_item[1:end:2]
+            for i in list_item:
+                file_write.write(i)
